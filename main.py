6bbf31a2bcc373060047dbe368aab7b6b7ed194a
mostafa-asefy <asefi.v@gmail.com>
2020-09-19 11:08:25 +0430

    L03 - Feature Detection

--- main.py
+++ main.py
@@ -9,8 +9,14 @@ success, videoImg = video_untold.read()
 hI, wI, cI = img_untold.shape
 videoImg = cv2.resize(videoImg, (wI, hI))
 
+orb = cv2.ORB_create(nfeatures=1000)
+kp, desc = orb.detectAndCompute(img_untold, None)
+img_untold = cv2.drawKeypoints(img_untold, kp, None)
+
 while True:
     success2, webcamImg = webcam.read()
+    kp2, desc2 = orb.detectAndCompute(webcamImg, None)
+    webcamImg = cv2.drawKeypoints(webcamImg, kp2, None)
     cv2.imshow('Webcam', webcamImg)
     cv2.imshow('Untold Image', img_untold)
     cv2.imshow('Untold Video', videoImg)
