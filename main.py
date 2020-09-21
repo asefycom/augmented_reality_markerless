06f79012da99a73917a452e5cc3f2f1e850e44ff
mostafa-asefy <asefi.v@gmail.com>
2020-09-21 09:54:14 +0330

    L04 - Matching

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 img_untold = cv2.imread('untold-ar.jpeg')
+img_untold = cv2.resize(img_untold, (0,0), fx=0.25, fy=0.25)
 video_untold = cv2.VideoCapture('untold-intro.mp4')
 webcam = cv2.VideoCapture(0)
 
@@ -11,14 +12,37 @@ videoImg = cv2.resize(videoImg, (wI, hI))
 
 orb = cv2.ORB_create(nfeatures=1000)
 kp, desc = orb.detectAndCompute(img_untold, None)
-img_untold = cv2.drawKeypoints(img_untold, kp, None)
+# img_untold = cv2.drawKeypoints(img_untold, kp, None)
+
+#If you want to select the best matches based on CrossCheck
+# bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+#If you want to select the best matches based on Ratio Test by D.Lowe
+bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+
 
 while True:
     success2, webcamImg = webcam.read()
     kp2, desc2 = orb.detectAndCompute(webcamImg, None)
-    webcamImg = cv2.drawKeypoints(webcamImg, kp2, None)
+    # webcamImg = cv2.drawKeypoints(webcamImg, kp2, None)
+    if desc2 is not None:
+        # matches = bf.match(desc, desc2)
+        # matches = sorted(matches, key=lambda x: x.distance)
+        # img3 = cv2.drawMatches(img_untold, kp, webcamImg, kp2, matches[:10], flags=2, outImg=None)
+        matches = bf.knnMatch(desc, desc2, k=2)
+        good = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                good.append([m])
+        img3 = cv2.drawMatchesKnn(img_untold, kp, webcamImg, kp2, good, flags=2, outImg=None)
+        cv2.imshow('Maching', img3)
+
     cv2.imshow('Webcam', webcamImg)
     cv2.imshow('Untold Image', img_untold)
     cv2.imshow('Untold Video', videoImg)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF==ord('q'):
+        break
+
+webcam.release()
+cv2.destroyAllWindows()
 
